96883a7f2cc09a1feee26ab4ca2a558eadbc1a79
Yunsang Choe <oddpoet@gmail.com>
2012-11-22 00:00:23 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh 
 
 # install vundle
+echo "Installing vundle (see https://github.com/gmarik/vundle)"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 # install .vimrc
+echo "Download default .vimrc "
 curl -o ~/.vimrc "https://raw.github.com/odd-poet/vimfiles/master/vimrc"
 
+echo "Done!"
